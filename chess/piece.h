6e90b024fa1f213c3e635c5ed377cab444bce80e
victoria92 <viki.hristova@gmail.com>
2014-02-21 18:57:18 +0200

    Make piece interface

--- chess/piece.h
+++ chess/piece.h
@@ -1,1 +1,14 @@
+class Piece;
 
+struct Field {
+    //bool empty;
+    Piece* piece;
+}
+
+class Piece {
+    string name;
+    Field place;
+    Piece(Field, name);
+    virtual Fields* possible_moves();
+    virtual void move();
+}
